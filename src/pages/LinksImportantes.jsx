680d700b0a66a59c2d7870684fe79179a3d6af72
Higor Zenaide <higorz.almeida@gmail.com>
2025-07-29 16:27:16 -0300

    filtro ajuste

--- src/pages/LinksImportantes.jsx
+++ src/pages/LinksImportantes.jsx
@@ -4,12 +4,12 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Badge } from '@/components/ui/badge';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Search } from "lucide-react";
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { 
   ExternalLink, 
+  Search,
   Plus, 
   Edit, 
   Trash2, 
